d1c1694dc8cb0ea9a68f78c70df05087d4415d0a
NF <jeaneudes.woto@hotmail.fr>
2024-03-13 17:46:42 +0100

    le header

---
+++ vues/gabarit.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <script src="https://kit.fontawesome.com/3b161c540c.js" crossorigin="anonymous"></script>
+  <link rel="stylesheet" type="text/css" href="../style/gabarit.css">
+</head>
+
+<body>
+  <header>
+    <div class="deconnexion">
+
+      <h4> Bonjour prenom </h4> <a href="">Déconnexion</a>
+    </div>
+
+    <div class="container">
+      <input type="checkbox" name="check" id="check">
+      <div class="logo-container">
+        <h3 class="logo"><img src="./images/logo.png" alt="" srcset=""></h3>
+      </div>
+
+      <div class="nav-btn">
+        <div class="nav-links">
+          <ul>
+            <li class="nav-link" style="--i: .6s">
+              <a href="#">Accueil</a>
+            </li>
+            <li class="nav-link" style="--i: .85s">
+              <a href="#">Expedition</i></a>
+
+            </li>
+            <li class="nav-link" style="--i: 1.1s">
+              <a href="#">Sav</a>
+
+            </li>
+            <li class="nav-link" style="--i: 1.35s">
+              <a href="#">Archives</a>
+            </li>
+          </ul>
+        </div>
+
+      </div>
+
+      <div class="hamburger-menu-container">
+        <div class="hamburger-menu">
+          <div></div>
+        </div>
+      </div>
+    </div>
+  </header>
+
+</body>
+
+</html>
