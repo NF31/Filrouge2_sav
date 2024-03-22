3679aac5ad0ddc4d4823dbf85032a696c6a0786e
Ivann-Elo <138256764+Ivann-Elo@users.noreply.github.com>
2024-03-22 15:43:41 +0100

    CREER TICKET OK

--- controler/tickets.php
+++ controler/tickets.php
@@ -1,5 +1,5 @@
 <?php
-    // Affichage des erreurs php 
+    //Affichage des erreurs php 
         ini_set('display_errors', 1);
         ini_set('display_startup_errors', 1);
         error_reporting(E_ALL);
@@ -7,14 +7,22 @@
     require_once '../model/modele.tickets.php';
     require_once '../model/ModelException.php';
 
+    if (session_status() === PHP_SESSION_NONE) {
+        session_start();
+    }
+     $nomSession = $_SESSION['nomUtilisateur'];
+     $roleSession = $_SESSION['roleUtilisateur'];
+     $idSession = $_SESSION['idUtilisateur'];
+
+    // print_r('<strong>Utilisateur connecté : </strong>'. $nomSession . '  ');
+    // print_r('<strong>Role utilisateur : </strong>'. $roleSession . ' ');
+    // print_r('<strong>Id utilisateur : </strong>'. $idSession . ' ');
+
 
     if(isset($_GET['action'])){
         $action = $_GET['action']; 
         $num_com = $_GET['num_com'];
         switch ($action) {
-            case 'showT_Exp':
-                break;
-            
             case 'createT_Exp':
                 $commande = getComById($num_com);
                 require_once '../vues/sideBar/vue_sideBarAll.php';
@@ -22,21 +30,32 @@
                 $contenu = $sideBarAll;
                 $contenu .= $affichCreatTexp;
                 require_once '../vues/gabarit.php';
-                break;
+            break;
+
             case 'confirmeCreatT_Exp':
                 $statut_ticket = $_GET['statut_ticket'];
                 $code_ticket = $_GET['code_ticket'];
-                $ticket = getComById($num_com);
-                require_once '../vues/sideBar/vue_sideBarAll.php';
-                require_once '../vues/affichageRes/vue_confTexp.php';
-                $contenu = $sideBarAll;
-                $contenu .= $affichConfTicket;
-                require_once '../vues/gabarit.php';
-                break; 
+            
+                // Vérification si un ticket avec le même code existe déjà
+                $controlTicket = controlTicket($code_ticket, $num_com);  
+                if(is_array($controlTicket) && $controlTicket['CODE_TICKET'] == $code_ticket){
+                    // Si le ticket existe déjà, afficher un message d'alerte et redirection
+                    echo "<script>alert('Un ticket $code_ticket a déjà été créé')</script>";
+                    echo "<script>window.location.href='commandes.php?num_com=$num_com&action=detail'</script>";
+                } else {
+                    // Si le ticket n'existe pas encore donc je créer du ticket
+                    $ticket = getComById($num_com);
+                    require_once '../vues/sideBar/vue_sideBarAll.php';
+                    require_once '../vues/affichageRes/vue_confTexp.php';
+                    $contenu = $sideBarAll . $affichConfTicket;
+                    require_once '../vues/gabarit.php';
+                }
+            break;
+                
             case 'creatOk':
                 $statut_ticket = $_GET['statut_ticket'];
                 $code_ticket = $_GET['code_ticket'];
-                $nouveauTicket = creatTicketExp( $code_ticket,  $num_com,  $statut_ticket);
+                $nouveauTicket = creatTicketExp( $code_ticket,  $num_com,  $statut_ticket, $idSession );
                 header('location: commandes.php?num_com='.$num_com.'&action=detail');
             break;        
         }
--- model/modele.tickets.php
+++ model/modele.tickets.php
@@ -5,13 +5,13 @@
     error_reporting(E_ALL);
 
     // Recupére la connection à la BDD
-    require('../config/connexion.php');
+    require_once('../config/connexion.php');
     
     ////////////////////////////////////////////////////
     //    RECHERCHER UNE COMMANDE AVEC SON NUM_COM    //
     ////////////////////////////////////////////////////
 
-  /**
+    /**
      * La fonction permet de récupérer une ligne de commande grâce à son numéro
      *
      * @param  int $num_com
@@ -35,15 +35,37 @@
      * @param  string $statut_ticket
      * @return void
      */
-    function creatTicketExp( string $code_ticket, int $num_com, string $statut_ticket){
+    function creatTicketExp( string $code_ticket, int $num_com, string $statut_ticket, int $idTech ){
         $bdd = getBdd(); 
-        $sql = "INSERT INTO TICKET_EXP (CODE_TICKET , NUM_COMMANDE, STATUT_TICKET)
-                VALUES (:code_ticket, :num_commande, :statut_commande )"; 
+        $sql = "INSERT INTO TICKET_EXP (CODE_TICKET , NUM_COMMANDE, STATUT_TICKET, ID)
+                VALUES (:code_ticket, :num_commande, :statut_commande, :idTech )"; 
         $curseur = $bdd->prepare($sql);
         $curseur->execute(array(
             'code_ticket' => $code_ticket, 
             'num_commande' => $num_com, 
-            'statut_commande' => $statut_ticket
+            'statut_commande' => $statut_ticket,
+            'idTech' => $idTech
         ));
+    }
+    
+    /**
+     * controlTicket cherche si il existe un ticket avec ces critères et retourne un false or true 
+     *
+     * @param  string $code_ticket
+     * @param  int $num_com
+     * @return 
+     */
+    function controlTicket(string $code_ticket, int $num_com){
+        $bdd = getBdd();
+        $sql = "SELECT * FROM `TICKET_EXP` WHERE NUM_COMMANDE LIKE :num_com AND CODE_TICKET LIKE :code_ticket ;"; 
+
+        $curseur = $bdd->prepare($sql); 
+        $curseur->execute(array(
+            'num_com' => $num_com,
+            'code_ticket' => $code_ticket
+        ));
+
+        $resultat = $curseur->fetch(PDO::FETCH_ASSOC);
+        return $resultat;
     }
 ?>
